bf06095acbd4273b6cb76caff164cdf414d43b7a
dennis.ludwig <dennis.ludwig@aoe.com>
2022-03-15 12:47:29 +0100

    fix(settings): add settings icon

--- app/components/Ul/index.tsx
+++ app/components/Ul/index.tsx
@@ -3,4 +3,5 @@ import { styled } from "~/stitches";
 export const Ul = styled("ul", {
   listStyleType: "none",
   padding: 0,
+  margin: 0,
 });
--- app/containers/Header/index.tsx
+++ app/containers/Header/index.tsx
@@ -1,14 +1,23 @@
 import { styled } from "~/stitches";
 import { Link } from "remix";
-import { Ul } from "~/components/Ul";
+import { IoMdSettings } from "react-icons/io";
+
+import { Ul as BaseUl } from "~/components/Ul";
 
 const StyledHeader = styled("header", {
   borderBottom: "1px solid $color",
+  padding: "$1 $1 $1 0",
 });
 
 const StyledLink = styled(Link, {
   textDecoration: "none",
   color: "$color",
+});
+
+const Ul = styled(BaseUl, {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
 });
 
 export const Header = () => (
@@ -19,7 +28,9 @@ export const Header = () => (
           <StyledLink to="/">Library</StyledLink>
         </li>
         <li>
-          <StyledLink to="/settings">Settings</StyledLink>
+          <StyledLink to="/settings">
+            <IoMdSettings />
+          </StyledLink>
         </li>
       </Ul>
     </nav>
