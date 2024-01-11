42f4c9ce8ff95fb866fd4156d383c0c3feef0ccb
Dennis Ludwig <gunslinger@sensonar.de>
2024-01-11 16:20:35 +0100

    chore: upgrade dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   "*.{json, md}": ["prettier --write"],
-  "package.json": ["generate:readme"],
   "app/**/*.{ts,tsx}": ["eslint", "prettier --write", "vitest related --run"],
 };
