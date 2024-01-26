0f93c066e0ef879caaadeaeafd1a5c7f0216fe06
Dennis Ludwig <gunslinger@sensonar.de>
2024-01-26 17:13:40 +0100

    chore: upgrade remix

--- remix.config.js
+++ remix.config.js
@@ -26,14 +26,4 @@ module.exports = {
     });
   },
   postcss: true,
-  future: {
-    v2_routeConvention: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_errorBoundary: true,
-    v2_normalizeFormMethod: true,
-    v2_dev: {
-      port: 8002,
-    },
-  },
 };
