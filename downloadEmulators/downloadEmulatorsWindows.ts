851def3ea0bfab20d425a148ef271edcffdf3a50
bmsuseluda <gunslinger@sensonar.de>
2025-02-08 07:50:11 +0100

    feat: update dolphin to 2412 on windows

--- downloadEmulators/downloadEmulatorsWindows.ts
+++ downloadEmulators/downloadEmulatorsWindows.ts
@@ -5,7 +5,7 @@ const emulatorDownloadsWindows: Partial<EmulatorDownloads> = {
   ares: "https://github.com/ares-emulator/ares/releases/download/v142/ares-windows-x64.zip",
   duckstation:
     "https://github.com/stenzek/duckstation/releases/download/v0.1-7371/duckstation-windows-x64-release.zip",
-  dolphin: "https://dl.dolphin-emu.org/releases/2407/dolphin-2407-x64.7z",
+  dolphin: "https://dl.dolphin-emu.org/releases/2412/dolphin-2412-x64.7z",
   ryujinx:
     "https://github.com/GreemDev/Ryujinx/releases/download/1.2.81/ryujinx-1.2.81-win_x64.zip",
 };
