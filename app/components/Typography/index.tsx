639af2296e33c926d7f9214c24fee3632c9217bf
Dennis Ludwig <dennis.ludwig@aoe.com>
2025-03-12 16:15:25 +0100

    refactor: use text-box-trim

--- app/components/Typography/index.tsx
+++ app/components/Typography/index.tsx
@@ -2,16 +2,7 @@ import { styled } from "../../../styled-system/jsx";
 
 export const Typography = styled("span", {
   base: {
-    _before: {
-      content: '""',
-      marginBottom: "-0.18em",
-      display: "table",
-    },
-    _after: {
-      content: '""',
-      marginTop: "-0.1em",
-      display: "table",
-    },
+    textBoxTrim: "trim-both",
   },
 
   variants: {
