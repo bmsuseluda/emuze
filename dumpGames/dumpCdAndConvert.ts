8ac07958109cee5ab3042cf3e239bd9fc7130f3b
Dennis Ludwig <gunslinger@sensonar.de>
2025-04-08 12:43:27 +0200

    feat: add scripts to dump and convert CDs and DVDs to chd files

--- dumpGames/dumpCdAndConvert.ts
+++ dumpGames/dumpCdAndConvert.ts
@@ -2,25 +2,36 @@ import { execFileSync } from "child_process";
 import { rmSync } from "node:fs";
 
 const dumpCdAndConvert = (name: string) => {
-  execFileSync("cdrdao", [
-    "read-cd",
-    ...["--datafile", `${name}.bin`],
-    ...["--driver", "generic-mmc:0x20000"],
-    ...["--device", "/dev/cdrom"],
-    ...["--read-raw", `${name}.toc`],
-  ]);
+  console.log("start dumping cd and creating bin and toc file", name);
+  execFileSync(
+    "cdrdao",
+    [
+      "read-cd",
+      ...["--datafile", `${name}.bin`],
+      ...["--driver", "generic-mmc:0x20000"],
+      ...["--device", "/dev/cdrom"],
+      ...["--read-raw", `${name}.toc`],
+    ],
+    { stdio: "inherit", encoding: "utf8" },
+  );
 
-  execFileSync("toc2cue", [`${name}.toc`, `${name}.cue`]);
+  console.log("creating cue file");
+  execFileSync("toc2cue", [`${name}.toc`, `${name}.cue`], {
+    stdio: "inherit",
+    encoding: "utf8",
+  });
 
-  execFileSync("chdman", [
-    "createcd",
-    ...["-i", `${name}.cue`],
-    ...["-o", `${name}.chd`],
-  ]);
+  console.log("creating chd file");
+  execFileSync(
+    "chdman",
+    ["createcd", ...["-i", `${name}.cue`], ...["-o", `${name}.chd`]],
+    { stdio: "inherit", encoding: "utf8" },
+  );
 
+  console.log("removing files");
   rmSync(`${name}.toc`);
   rmSync(`${name}.bin`);
   rmSync(`${name}.cue`);
 };
 
-dumpCdAndConvert("TOBAL No.1");
+dumpCdAndConvert("Disruptor");
