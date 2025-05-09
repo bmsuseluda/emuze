f11e57e3599e71d9d3dbcc7bc2bc7c18b0f8b4dd
Dennis Ludwig <gunslinger@sensonar.de>
2025-05-09 15:49:12 +0200

    chore: upgrade remix to latest version with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,89 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import type { DefineRouteFunction } from "@remix-run/dev/dist/config/routes";
-import { installGlobals } from "@remix-run/node";
-
-const createErrorDialog = (route: DefineRouteFunction, id: string) => {
-  route("errorDialog", "customRoutes/categories.$category.errorDialog.tsx", {
-    id: `${id}ErrorDialog`,
-  });
-};
-
-const createGameDialog = (route: DefineRouteFunction, id: string) => {
-  route(
-    ":gameId",
-    "customRoutes/categories.$category.$gameId.tsx",
-    {
-      id: `${id}GameDialog`,
-    },
-    () => {
-      createErrorDialog(route, `${id}GameDialog`);
-    },
-  );
-};
-
-const createSettingsRoutes = (route: DefineRouteFunction, id: string) => {
-  route(
-    "settings",
-    "customRoutes/categories.$category.settings.tsx",
-    {
-      id: `${id}Settings`,
-    },
-    () => {
-      route("", "customRoutes/categories.$category.settings._index.tsx", {
-        index: true,
-        id: `${id}SettingsIndex`,
-      });
-      route(
-        "general",
-        "customRoutes/categories.$category.settings.general.tsx",
-        {
-          id: `${id}SettingsGeneral`,
-        },
-        () => {
-          createErrorDialog(route, `${id}SettingsGeneral`);
-        },
-      );
-      route(
-        "appearance",
-        "customRoutes/categories.$category.settings.appearance.tsx",
-        {
-          id: `${id}SettingsAppearance`,
-        },
-      );
-      route("about", "customRoutes/categories.$category.settings.about.tsx", {
-        id: `${id}SettingsAbout`,
-      });
-    },
-  );
-};
-
-const createCategoriesRoutes = (route: DefineRouteFunction) => {
-  route("categories", "customRoutes/categories.tsx", {}, () => {
-    route("lastPlayed", "customRoutes/categories.lastPlayed.tsx", () => {
-      const id = "lastPlayed";
-      route(
-        ":category/:gameId",
-        "customRoutes/categories.$category.$gameId.tsx",
-        {
-          id: `${id}GameDialog`,
-        },
-        () => {
-          createErrorDialog(route, `${id}GameDialog`);
-        },
-      );
-      createSettingsRoutes(route, id);
-      createErrorDialog(route, id);
-    });
-    route(":category", "customRoutes/categories.$category.tsx", () => {
-      createGameDialog(route, "category");
-      createSettingsRoutes(route, "category");
-      createErrorDialog(route, "category");
-    });
-  });
-};
-
-installGlobals();
 
 declare module "@remix-run/node" {
   interface Future {
@@ -108,20 +24,13 @@ export default defineConfig({
     remix({
       appDirectory: "app",
       serverModuleFormat: "cjs",
-      routes(defineRoutes) {
-        return defineRoutes((route) => {
-          route("/", "customRoutes/_index.tsx", { index: true });
-          createCategoriesRoutes(route);
-          createSettingsRoutes(route, "initial");
-          route("/invalidateCaches", "customRoutes/invalidateCaches.tsx");
-        });
-      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
+        v3_routeConfig: true,
       },
     }),
   ],
