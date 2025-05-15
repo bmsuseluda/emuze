bc13d9e0813eb2eec5498db215b710b65b4e2bd2
Dennis Ludwig <gunslinger@sensonar.de>
2025-05-15 14:43:14 +0200

    chore: merge from release branch

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config";
 
+// This is intentional to remove plugins from testing
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { plugins, ...viteConfigWithoutPlugins } = viteConfig;
+
 export default mergeConfig(
-  viteConfig,
+  viteConfigWithoutPlugins,
   defineConfig({
     test: {
       globals: true,
