1474afc230a0edab3376634578f6e78577909ca0
Dennis Ludwig <gunslinger@sensonar.de>
2025-07-24 16:46:51 +0200

    fix(ares145): fix closing via gamepad

--- app/server/closeGame.server.ts
+++ app/server/closeGame.server.ts
@@ -11,7 +11,7 @@ const killChildProcess = () => {
   log("debug", "kill process");
 
   if (isGameRunning() && gameIsRunningChildProcess?.pid) {
-    kill(gameIsRunningChildProcess.pid, "SIGKILL", (err) => {
+    kill(gameIsRunningChildProcess.pid, "SIGHUP", (err) => {
       if (err) {
         log("error", "Failed to kill process:", err);
       }
