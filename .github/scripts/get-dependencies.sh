00b272a6c7eb302001da2e8d34d246a7cd87fb59
bmsuseluda <gunslinger@sensonar.de>
2026-01-29 16:32:27 +0100

    chore: reduce appImage size

--- .github/scripts/get-dependencies.sh
+++ .github/scripts/get-dependencies.sh
@@ -24,10 +24,14 @@ get-debloated-pkgs --add-common --prefer-nano
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
           . "$HOME/.nvm/nvm.sh"
+          export NVM_DIR="$HOME/.nvm"
+          [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
           nvm install 22
           nvm use
           node -v
+          npm -v
           npm install -g corepack
+          yarn -v
           yarn
           yarn app:distLinux --publish=never
           yarn shrinkBundleSize
