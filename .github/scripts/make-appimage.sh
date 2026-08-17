0fb95d7f715ae977fa53a6bc2fa8fcb8f28377e1
bmsuseluda <gunslinger@sensonar.de>
2026-08-17 12:58:34 +0200

    chore: use appimageupdate

--- .github/scripts/make-appimage.sh
+++ .github/scripts/make-appimage.sh
@@ -3,7 +3,7 @@
 set -eu
 
 ARCH=x86_64
-VERSION=$(node -p "require('../../package.json').version")
+VERSION=$(node -p "require('./package.json').version")
 export ARCH VERSION
 export OUTPATH=./dist
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*.AppImage.zsync"
