625ef4454e342833f5e77438fd662f781dc23072
bmsuseluda <gunslinger@sensonar.de>
2026-08-17 16:06:52 +0200

    chore: use appimageupdate

--- .github/scripts/get-dependencies.sh
+++ .github/scripts/get-dependencies.sh
@@ -6,23 +6,16 @@ ARCH=x86_64
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm  \
-            python       \
-            nss          \
-            at-spi2-core \
-            nodejs-lts-jod \
+            nodejs-lts-krypton \
             npm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-node -v
-npm -v
 npm install -g corepack
-yarn -v
-
 yarn
-yarn app:distLinux
+yarn app:dirLinux
 yarn shrinkBundleSize
 
 mkdir -p ./AppDir/bin
@@ -31,8 +24,7 @@ mv -v  dist/linux-unpacked/biosOpenSource ./AppDir
 mv -v  dist/linux-unpacked/CHANGELOG.md ./AppDir
 mv -v  dist/linux-unpacked/updater ./AppDir
 
-cd dist && ./emuze-*.AppImage --appimage-extract && cd ..
-cp  dist/squashfs-root/.DirIcon ./AppDir
-cp  dist/squashfs-root/emuze.desktop ./AppDir
+cp -v  .github/scripts/.DirIcon ./AppDir
+cp -v  .github/scripts/emuze.desktop ./AppDir
 
 cp -rv dist/linux-unpacked/* ./AppDir/bin/
--- .github/scripts/make-appimage.sh
+++ .github/scripts/make-appimage.sh
@@ -13,3 +13,7 @@ quick-sharun ./AppDir/bin/*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --test ./dist/*.AppImage
