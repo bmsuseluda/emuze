988415a8288ac6ec0375a9ff14ec209aaba608c9
bmsuseluda <gunslinger@sensonar.de>
2026-09-21 12:04:47 +0200

    fix: tests

--- .github/scripts/get-dependencies.sh
+++ .github/scripts/get-dependencies.sh
@@ -2,17 +2,10 @@
 
 set -eu
 
-echo "Installing package dependencies..."
-echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm  \
-            nodejs-lts-krypton \
-            npm \
-
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-npm install -g corepack
 yarn
 yarn app:dirLinux
 yarn shrinkBundleSize
